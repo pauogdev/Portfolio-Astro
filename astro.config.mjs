5fd0e65a3fd8c0d73d48d8a730befdeca00fdf14
pauortega <pauortegarcia@outlook.es>
2024-03-02 02:12:25 +0100

    Eliminacion componente react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  output:'server',
-  adapter: node({
-    mode:"standalone"
-  }),
-  integrations: [tailwind(), react(), compressor({gzip: true})],  
+  
+  integrations: [tailwind(), compressor({gzip: true})],  
 });
